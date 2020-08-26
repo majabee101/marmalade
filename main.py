7cefaa505d6ebbc9a06961e8fe864b6cc27e8b11
majabee101 <61654115+majabee101@users.noreply.github.com>
2020-08-26 12:30:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__, static_folder="./templates", static_url_path="/templates")
+app.debug=True
+
+username = "Samj"
+currentBudget = 350
+
+
+@app.route("/")
+def homePage():
+
+    print("User connected to the homepage")
+
+
+    return render_template("homepage.html", user= username, budget=currentBudget)
+
+
+@app.route("/budget", methods=["GET", "POST"])
+def changeBudget():
+    if request.method == "POST":
+        global currentBudget
+        newBudget = request.form.get("new_budget")
+        print(newBudget)
+        currentBudget = newBudget
+        return jsonify({"new_budget":newBudget})
+
+    return render_template("budget.html", user= username, budget=currentBudget)
+
+
+
+
+if __name__ == "__main__":
+    app.run()
