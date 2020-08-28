1863843c3617fd8fa1858caeaa59c833c7eb3d4f
majabee101 <61654115+majabee101@users.noreply.github.com>
2020-08-28 12:10:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ currentBudget = 350
 
 
 @app.route("/")
+def mainPage():
+
+    print("User connected to the mainpage")
+
+
+    return render_template("mainpage.html", user= username, budget=currentBudget)
+
+@app.route("/homepage", methods=["GET", "POST"])
 def homePage():
 
-    print("User connected to the homepage")
+    print("user connected to homepage")
 
-
-    return render_template("homepage.html", user= username, budget=currentBudget)
+    return render_template("homepage.html")
 
 
 @app.route("/budget", methods=["GET", "POST"])
